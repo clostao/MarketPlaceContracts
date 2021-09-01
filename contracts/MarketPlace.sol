5125492d6f17902d7e3850a56f09b3a27e7f16c2
clostao <hastro1990@gmail.com>
2021-09-01 22:38:23 +0200

    UPD: Updated selling ang buying products functions, in order to transfer NFTs

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+abstract contract NFTManager {
+  function safeTransferFrom(address, address, uint256) public virtual;
+}
+
 contract MarketPlace {
   
   struct Product {
-    string name;
-    string description;
-    bool selling;
-    uint256 productId;
-    address seller;
-    address soldTo;
+    uint256 tokenId;
+    address owner;
+    bool sold;
     uint256 price;
   }
 
   string name;
 
+  address nftContract;
   uint256 nextProductId = 1;
   uint256 productsToSell = 0;
 
@@ -25,32 +27,36 @@ contract MarketPlace {
   event LogProductInSell(Product product);
   
 
-  constructor(string memory _name) {
+  constructor(string memory _name, address _contract) {
+    nftContract = _contract;
     name = _name;
     nextProductId = 1;
   }
 
   function addProductToSell( 
-    string memory _name,
-    string memory _description,
+    uint256 _tokenId,
     uint256 _price
     ) external {
       
     // Setting Product struct fields
-    Product memory product = Product(_name, _description, true, nextProductId, msg.sender, address(0), _price);
+    Product memory product = Product(_tokenId, msg.sender, false, _price);
 
     // Altering state
-    products[nextProductId] = product;
+    uint256 productId = nextProductId;
+    products[productId] = product;
     emit LogProductInSell(product);
     nextProductId = nextProductId + 1;
     productsToSell = productsToSell + 1;
+
+    // Transfer NFT
+    NFTManager(nftContract).safeTransferFrom(msg.sender, nftContract, _tokenId);
   }
 
   function getProductsInSell() public view returns (Product[] memory) {
     uint j = 0;
     Product[] memory _products = new Product[](productsToSell);
     for (uint256 i = 1; i < nextProductId && j < productsToSell; i++) {
-      if (products[i].selling == true) {
+      if (products[i].sold == false) {
         _products[j] = products[i];
         j++;
       }
@@ -69,16 +75,18 @@ contract MarketPlace {
     require(productId > 0 && productId < nextProductId, "Product doesn't exist");
 
     // If sell conditions are met
-    require(products[productId].selling, "The article has already been sold.");
-    require(products[productId].seller != msg.sender, "Seller and customer cannot be the same account.");
+    require(!products[productId].sold, "The article has already been sold.");
+    require(products[productId].owner != msg.sender, "Seller and customer cannot be the same account.");
     require(products[productId].price == uint256(msg.value), "Price and tx value should meet.");
-    require(products[productId].selling == true, "Product is not longer in sell");
 
     // Then we process the tx
-    payable(products[productId].seller).transfer(products[productId].price);
-    products[productId].selling = false;
-    products[productId].soldTo = msg.sender;
+    payable(products[productId].owner).transfer(products[productId].price);
+    products[productId].sold = true;
+    products[productId].owner = msg.sender;
     productsToSell = productsToSell - 1;
+
+    // Transfer NFT
+    NFTManager(nftContract).safeTransferFrom(nftContract, msg.sender, products[productId].tokenId);
 
     // And emit the correspondant event
     emit LogProductBought(productId);
